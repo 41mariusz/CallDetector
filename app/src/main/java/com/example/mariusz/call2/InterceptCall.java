c0b8526086a3677a64a35ecceade6688efd2afea
Mona Lisa <41mariusz@gmail.com>
2019-03-21 21:50:12 +0100

    CallDetector

--- app/src/main/java/com/example/mariusz/call2/InterceptCall.java
+++ app/src/main/java/com/example/mariusz/call2/InterceptCall.java
@@ -4,25 +4,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.Toast;
 
 public class InterceptCall extends BroadcastReceiver {
-    @Override
     public void onReceive(Context context, Intent intent) {
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
         try {
-            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)) {
+            if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)) {
                 String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                Toast.makeText(context,  "Ringing! " + number, Toast.LENGTH_LONG).show();
+
+                Toast.makeText(context, "Ringing! " + number + " ## ", Toast.LENGTH_LONG).show();
+
+                Log.d("Ringing: ", "> " + number);
+                Log.d("JSON", "why");
+
             }
-            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-                Toast.makeText(context, "Recived!", Toast.LENGTH_LONG).show();
+            if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                Log.d("Call: ", "Recivied");
             }
-            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_IDLE)) {
-                Toast.makeText(context, "IDLE!", Toast.LENGTH_LONG).show();
+            if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_IDLE)) {
+                Log.d("Call: ", "IDLE");
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
+
--- app/src/main/java/com/example/mariusz/call2/MainActivity.java
+++ app/src/main/java/com/example/mariusz/call2/MainActivity.java
@@ -2,22 +2,23 @@ package com.example.mariusz.call2;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_PHONE_STATE)) {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_PHONE_STATE)) {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
             } else {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
@@ -25,21 +26,14 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // do nothing
         }
+
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:{
-                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(this, "No permission granted!", Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
-        }
+    public void setTxt(String newText) {
+        text = (TextView) findViewById(R.id.textView1);
+        text.setText(newText);
     }
+
+
 }
+
